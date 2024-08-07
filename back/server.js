3ff1ee1733450198db08be77659b72732d5e5ca9
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2024-08-07 10:42:44 -0300

    servidor

---
+++ back/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const BodyParser = require('body-parser');
+const sql = require('./database/mysqldb')
+const server = express();
+
+const port = 9080;
+const ip = '127.0.0.1';
+
+server.use(cors({
+    origin: '*'
+}))
+
+server.use(BodyParser.json());
+
+server.listen(port, ip, () => {
+    console.log(`Servidor Rodando na porta ${port}`)
+})
+
+server.get('/receitas', (req, res) => {
+let id_receita = req.query.id_receita;
+sql.query('SELECT * FROM receita WHERE id_receita = ?', [id_receita], (error, result) => {
+    res.json(result)
+})
+})
+
+server.get('/ingredientes', (req, res) => {
+ let id_receita = req.query.id_receita;
+ sql.query('SELECT * FROM ingredientes WHERE id_receita = ?',[id_receita], (error, result) => {
+    res.json(result)
+ })
+})
+
+server.get('/quantidadeReceitas', (req, res) => {
+    // Consulta feita apenas para obter a quantidade total de receitas.
+    sql.query('SELECT id_receita FROM receita', (error, result) => {
+        res.json(result);
+    })
+})
+
+
